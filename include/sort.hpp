ae576a9bc3e60f5ebf8ec816d5541192766546f6
Kian Ashrafganjouei <Kian.Asrafganjouei@gmail.com>
2024-09-08 00:31:21 -0400

    clang format

--- include/sort.hpp
+++ include/sort.hpp
@@ -1,9 +1,11 @@
 #include <vector>
 
 class Sort {
-    public:
-        static void insertionSort(std::vector<int> &vect);
-    private:
-        static void visualizeArray(const std::vector<int>& vect, int highlightIndex = -1, int compareIndex = -1);
-        static void clearScreen();
+public:
+  static void insertionSort(std::vector<int> &vect);
+
+private:
+  static void visualizeArray(const std::vector<int> &vect,
+                             int highlightIndex = -1, int compareIndex = -1);
+  static void clearScreen();
 };
